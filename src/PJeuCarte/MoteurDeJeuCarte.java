30efe9126a02f15598244749d02cc86d2d3f0dd5
GuilhemGabrielli <guilhem.gabrielli@campus.academy>
2022-12-28 14:04:31 +0100

    Correctifs jeu infini, perfectionner ajout carte

--- src/PJeuCarte/MoteurDeJeuCarte.java
+++ src/PJeuCarte/MoteurDeJeuCarte.java
@@ -4,13 +4,13 @@ import PCarte.Carte;
 import PCarte.Couleur;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class MoteurDeJeuCarte {
 
     private List<Carte> a_cartes;
-    private List<Carte> a_cartes2;
 
     MoteurDeJeuCarte(JeuCarte jeuCarte) {
         a_cartes = new ArrayList<>();
@@ -34,34 +34,13 @@ public class MoteurDeJeuCarte {
             a_cartes.add(carte3);
             a_cartes.add(carte4);
         }
-
-        a_cartes2 = new ArrayList<>();
-        Carte carte1 = new Carte("2", Couleur.PIQUE);
-        Carte carte2 = new Carte("3", Couleur.COEUR);
-        Carte carte3 = new Carte("4", Couleur.CARREAU);
-        Carte carte4 = new Carte("5", Couleur.TREFLE);
-        Carte carte5 = new Carte("6", Couleur.CARREAU);
-        Carte carte6 = new Carte("7", Couleur.TREFLE);
-        a_cartes2.add(carte1);
-        a_cartes2.add(carte2);
-        a_cartes2.add(carte3);
-        a_cartes2.add(carte4);
     }
 
 
     public void jouer(String nomJeu) {
         if (nomJeu == "Bataille") {
-            melangerCarte(this.a_cartes);
-
+            Collections.shuffle(this.a_cartes);
             JeuBataille jeuBataille = new JeuBataille(this.a_cartes, "j1", "j2");
-        }
-    }
-
-    private void melangerCarte(List<Carte> cartes) {
-        for (int i = 0; i < cartes.size(); i++) {
-            Carte carteAMelanger = cartes.remove(cartes.size()-1);
-            Random rand = new Random();
-            cartes.add(rand.nextInt(0, cartes.size()), carteAMelanger);
         }
     }
 
